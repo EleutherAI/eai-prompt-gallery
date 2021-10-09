e5b8dcd6b21a36ef6d6c44e16ae407503c53e063
Janko <janko.prester@gmail.com>
2021-10-09 23:22:32 +0200

    fix env finally

--- src/pages/text/SingleItemText.js
+++ src/pages/text/SingleItemText.js
@@ -16,7 +16,7 @@ export default function SingleItem() {
 
   const [selectedItem, setSelectedItem] = useState([]);
   useEffect(() => {
-    fetch(`${proccess.env.REACT_APP_DATA_HOST}/api/textPrompts.json`)
+    fetch(`${process.env.REACT_APP_DATA_HOST}/api/textPrompts.json`)
       .then((response) => {
         return response.json();
       })
