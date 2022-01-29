bb8aac51e641378169b7b171590885a0df0cb8a5
Janko <janko.prester@gmail.com>
2022-01-29 23:04:49 +0100

    move to yarn and create thumbnails during build

--- createThumbnails.js
+++ createThumbnails.js
@@ -7,10 +7,16 @@ function getExtension(filename) {
   return parts[parts.length - 1];
 }
 const directoryPath = path.join(__dirname, "/public/api/art");
+
 fs.readdir(directoryPath + "/optimised/", function (err, files) {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
+
+  if (!fs.existsSync(directoryPath + "/thumbnails/")) {
+    fs.mkdirSync(directoryPath + "/thumbnails/");
+  }
+
   files.forEach(function (file) {
     if (getExtension(file) === "png") {
       sharp(directoryPath + "/optimised/" + file)
